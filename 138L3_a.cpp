950d088e35dbd6843fe1901ecf80414cef924df2
rusty <aprajun@gmail.com>
2018-01-24 10:12:39 +0530

    Lab Programs

---
+++ 138L3_a.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "138L3_b.cpp"
+using namespace std;
+
+int e=1;
+
+int main() {	
+	testExtern();	
+	return 0;
+}
---
+++ 138L3_b.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+void testExtern() {
+	extern int e;
+	cout<<"Testing Extern Variable e : "<<e<<endl;
+}
